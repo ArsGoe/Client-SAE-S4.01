40c10bd21d6f6e0c4a368bb38c6800b3a698d91a
Félix Silvert <felix_silvert@ens.univ-artois.fr>
2024-04-04 10:54:33 +0200

    création du service et de l'interface de Evenement, close #3

---
+++ src/app/models/evenement.ts
@@ -0,0 +1,18 @@
+export interface Evenement {
+  id: number;
+  titre: string;
+  description: string;
+  dateEvent: Date;
+  type: TypeEvenement;
+  lieu: Lieu;
+  artistes: Artiste[];
+  prix: Prix[];
+}
+
+export enum TypeEvenement {
+  THEATRE,
+  CINEMA,
+  CONCERT,
+  FESTIVAL,
+  COMPETITION
+}
