8710bbcfdd77258d75e75188b64959f9bb0c6f7b
Félix Silvert <felix_silvert@ens.univ-artois.fr>
2024-04-05 10:49:24 +0200

    Finalisation du bandeau de navigation, close #20

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {ProfilComponent} from "./profil/profil.component";
+import {ListeEvenementComponent} from "./liste-evenement/liste-evenement.component";
+import {ListeReservationComponent} from "./liste-reservation/liste-reservation.component";
 
 export const routes: Routes = [
-
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-
+  {path: 'profil', component: ProfilComponent},
+  {path: 'liste-evenement', component: ListeEvenementComponent},
+  {path: 'liste-reservation', component: ListeReservationComponent},
 ];
